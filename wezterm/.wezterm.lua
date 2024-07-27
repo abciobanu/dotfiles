3af12c7adae0a66891d88587c2e86b0cea7d7c01
Andrei-Bogdan Ciobanu <cb.andreibogdan@gmail.com>
2024-07-27 18:11:38 +0300

    Revert "wezterm: Increase the font size"
    This reverts commit 8436100277716d9cdf2563c66886b29ff0103e22.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,6 +5,5 @@ local config = wezterm.config_builder()
 config.enable_tab_bar = false
 config.color_scheme = "Gruvbox dark, hard (base16)"
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 16
 
 return config

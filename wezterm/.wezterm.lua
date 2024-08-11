bce3cf49c6fec6839e7b832cbb585303dd83323a
Andrei-Bogdan Ciobanu <cb.andreibogdan@gmail.com>
2024-08-11 10:37:25 +0300

    wezterm: Disable the title bar but enable the resizable border

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.color_scheme = "Gruvbox dark, hard (base16)"
 config.font = wezterm.font("JetBrains Mono")

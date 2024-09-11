ca5b823f03cd40143bc39a91bc950029aab5452d
Andrei-Bogdan Ciobanu <cb.andreibogdan@gmail.com>
2024-09-11 12:39:08 +0300

    wezterm: Set font size to 14

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,5 +6,6 @@ config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.color_scheme = "Gruvbox dark, hard (base16)"
 config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14.0
 
 return config

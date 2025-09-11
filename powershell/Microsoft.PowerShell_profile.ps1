00098e16293790112925e54ae6e2eba1509c3612
Andrei-Bogdan Ciobanu <cb.andreibogdan@gmail.com>
2025-09-11 11:24:41 +0300

    powershell: update oh-my-posh theme

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
-oh-my-posh init pwsh --config 'robbyrussell' | Invoke-Expression
+oh-my-posh init pwsh --config 'half-life' | Invoke-Expression
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
